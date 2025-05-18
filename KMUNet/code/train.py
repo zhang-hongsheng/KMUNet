e9451e7db8d0e60f115fc13f63bc4d4e640cfd6f
zhang-hongsheng <1789483835@qq.com>
2025-05-18 16:38:22 +0800

    Update train.py

--- KMUNet/code/train.py
+++ KMUNet/code/train.py
@@ -86,7 +86,7 @@ def train(args):
     #                   lr=0.001,
     #                   betas=(0.9, 0.999),
     #                   eps=1e-8,
-    #                   weight_decay=0.0001,
+    #                   weight_decay=0.01,
     #                   amsgrad=False)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
